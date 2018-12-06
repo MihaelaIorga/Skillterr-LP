b26beadadef4aa6c7fc9eaf6980cac3ee99725c5
Mihaela Laura Iorga <mihaela@Unitys-MacBook-Pro-3.local>
2018-12-06 19:45:51 +0100

    all-the-stuff

---
+++ connect.php
@@ -0,0 +1,39 @@
+<?php
+
+$email = filter_input(INPUT_POST, 'email');
+if (!empty($email)){
+$host = "skillterr.com.mysql";
+    $dbusername = "skillterr_com";
+    $dbpassword = "NHjABpZVWVHNpnyr29YKutD2";
+    $dbname = "skillterr_com";
+
+    // Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+    if (mysqli_connect_error()){
+        die('Connect Error ('. mysqli_connect_error() .') '
+            . mysqli_connect_error());
+    }
+else {
+$sql = "INSERT INTO members (email_address)
+values ('$email')";
+if ($conn->query($sql)){
+echo "New record is inserted successfully";
+}
+else{
+echo "Error: " . $sql . "<br>".$conn->error;
+}
+$conn->close();
+}
+
+
+
+
+
+
+}
+else{
+    echo "Please fill in your email";
+    die();
+}
+?>
