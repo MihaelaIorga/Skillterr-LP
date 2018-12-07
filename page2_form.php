c80eea8ddfc619727c554b91b2c2be64e7be0c8f
Mihaela Laura Iorga <mihaela@Unitys-MacBook-Pro-3.local>
2018-12-07 17:13:48 +0100

    final-php

--- page2_form.php
+++ page2_form.php
@@ -2,17 +2,31 @@
 session_start(); // Session starts here.
 
  //register session variables
-      $_SESSION ['email_address'];
+
+
+if  (!empty($_POST["email_address"])) {
+
+$_SESSION ['email_address'];
+
+
         //store posted values in the session
-        $_SESSION ['email_address'] = $_POST ['email_address']
+        $_SESSION ['email_address'] = $_POST ['email_address'];
+
+}
+
+else{
+    header('Location: index.html');
+    }
+
 ?>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home</title>
+        <title>Swap Skills - Skillterr</title>
         <link rel="stylesheet" href="https://use.fontawesome.com/826a7e3dce.css" media="all">
         <link rel="stylesheet" href="details.css">
+        <link rel="shortcut icon" type="image/ico" href="images/fav-raw.ico">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 
     </head>
@@ -36,8 +50,10 @@ session_start(); // Session starts here.
                         <form class="personal_details" method="POST" action="page3_form.php">
                             <input class="first_name" type="text" placeholder="First Name" name="first_name"><br>
                             <input class="last_name" type="text" placeholder="Last Name" name="last_name"><br>
-                            <input class="submit-join" type="submit" value="Welcome" name="submit-join">
+                            <div class="buttons-align">
+                            <input class="submit-join" type="submit" value="Welcome" name="submit-join"><br>
                             <input class="skip" type="submit" value="skip" name="skip">
+                            </div>
                         </form>
                     </div>
                     <div class="Headline-Image"></div>
